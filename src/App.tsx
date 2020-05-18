19d2f68435aea83c4b9944e764e5e990cef4999c
sjelzinga <sje.elzinga@gmail.com>
2020-05-18 13:40:48 +0200

    Add basic sass setup
    Basic Sass setup with variable file and button example file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { Test } from "components/Test";
+import { Button } from "components/layout/Button/Button";
 
 function App() {
   return (
     <div className="App">
       <Test></Test>
+      <Button></Button>
     </div>
   );
 }
